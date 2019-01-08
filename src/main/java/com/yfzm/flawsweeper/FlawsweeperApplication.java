d3581f65a06f26bdf67fcc965ed8f2414022e348
Li Hao <yfzm1223@sjtu.edu.cn>
2019-01-08 19:14:46 +0800

    add cros

--- src/main/java/com/yfzm/flawsweeper/FlawsweeperApplication.java
+++ src/main/java/com/yfzm/flawsweeper/FlawsweeperApplication.java
@@ -2,6 +2,9 @@ package com.yfzm.flawsweeper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class FlawsweeperApplication {
@@ -10,5 +13,14 @@ public class FlawsweeperApplication {
         SpringApplication.run(FlawsweeperApplication.class, args);
     }
 
+    @Configuration
+    public class CorsConfig extends WebMvcConfigurerAdapter {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedOrigins("*")
+                    .allowedMethods("GET", "HEAD", "POST","PUT", "DELETE", "OPTIONS")
+                    .allowCredentials(true).maxAge(3600);
+        }
+    }
 }
 
